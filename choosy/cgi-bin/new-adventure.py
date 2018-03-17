4d4d5221fc7123bd1539530d5aff08e7e4d0492c
Hunter <htncato@gmail.com>
2018-03-17 02:35:17 -0700

    refactoring!

--- choosy/cgi-bin/new-adventure.py
+++ choosy/cgi-bin/new-adventure.py
@@ -1,13 +1,24 @@
+#!C:\Users\Mimir\AppData\Local\Programs\Python\Python36\python.exe
+
 import cgi
 import os
 import cgitb
 cgitb.enable()
 
 
+
+
 form = cgi.FieldStorage()
 
-path = './adventures/{}'.format(form.getvalue('adventure-title'))
+title = form.getvalue('adventure-title')
+description = form.getvalue('adventure-description')
+
+path = 'web/adventures/' + title
+adventure_location = path + '/' + title + '.html'
+
+
 if os.path.isdir(path):
+
     print("""
         <!DOCTYPE html>
         <html>
@@ -16,39 +27,63 @@ if os.path.isdir(path):
           <title>Adventure Creation</title>
           <link rel='stylesheet' href='/css/style.css'>
         </head>
-
         <body>
           <div>
             <form action='/cgi-bin/new-adventure.py' method='post'>
-              There is already an adventure with that title, try again:<br>
-              <input type='text' name='adventure-title'>
-              <input type='submit' value='Save Story'>
+
+              There is already an adventure with that title, try again:<br><br>
+
+              Adventure title:<br>
+              <input type='text' name='adventure-title'><br><br>
+
+              Adventure description:<br>
+              <textarea name='adventure-description'></textarea><br><br>
+
+              <input type='submit' value='Save Adventure'>
             </form>
           </div>
         </body>
         </html>
     """)
+
 else:
+
     os.makedirs(path, exist_ok=True)
+
+    homepage = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset='UTF-8'>
+          <title>{}</title>
+          <link rel='stylesheet' href='/css/style.css'>
+        </head>
+        <body>
+          <div>
+            {}
+          </div>
+        </body>
+        </html>
+    """.format(title, description)
+
+    with open(adventure_location, 'w') as adventure_home_page:
+
+        adventure_home_page.write(homepage)
+
+
     print("""
         <!DOCTYPE html>
         <html>
         <head>
           <meta charset='UTF-8'>
+          <meta http-equiv='refresh' content='1; url=/{}'
           <title>Room Creation</title>
           <link rel='stylesheet' href='/css/style.css'>
         </head>
-
         <body>
           <div>
-            <form action='/cgi-bin/new-room.py' method='post'>
-              Adventure was created successfully, now we can create pages<br>
-              The first node in an adventure is always titled 'Home'<br>
-              Describe the opening node below:<br>
-              <textarea name='home-room-description' rows='20' cols='60'></textarea><br>
-              <input type='submit' value='Save Description'>
-            </form>
+            Adventure created successfully, redirecting...
           </div>
         </body>
         </html>
-    """)
+    """.format(adventure_location))
